2fc9829e77f0808c07350613d1103a960034ccbb
kocopepo <martinkocour1102@gmail.com>
2016-05-01 23:08:52 +0200

    Added Content Provider Activity.

---
+++ app/src/main/java/com/touchpo/android/dotykacka_meetup/activity/ContentProviderActivity.java
@@ -0,0 +1,15 @@
+package com.touchpo.android.dotykacka_meetup.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.touchpo.android.dotykacka_meetup.R;
+
+public class ContentProviderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_provider);
+    }
+}
