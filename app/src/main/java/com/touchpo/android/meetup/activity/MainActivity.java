eb37278377c2baef31f524e08a1b41649639f005
kocopepo <martinkocour1102@gmail.com>
2016-05-01 22:59:31 +0200

    Meetup topic recycler view.

---
+++ app/src/main/java/com/touchpo/android/meetup/activity/MainActivity.java
@@ -0,0 +1,45 @@
+package com.touchpo.android.meetup.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.touchpo.android.dotykacka_meetup.R;
+import com.touchpo.android.meetup.adapter.MeetupAdapter;
+import com.touchpo.android.meetup.adapter.MeetupAdapter.MeetupTopic;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements MeetupAdapter.MeetupItemActionListener {
+
+    @BindView(android.R.id.list)
+    RecyclerView mList;
+
+    private MeetupAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        initList();
+    }
+
+    private void initList() {
+        mAdapter = new MeetupAdapter(this);
+        mAdapter.setListener(this);
+        mList.setHasFixedSize(true);
+        mList.setLayoutManager(new LinearLayoutManager(this));
+        mList.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onClickListener(int position) {
+        MeetupTopic topic =  mAdapter.getMeetupTopic(position);
+        startActivity(new Intent(this, topic.activityClass));
+    }
+}
---
+++ app/src/main/java/com/touchpo/android/meetup/adapter/MeetupAdapter.java
@@ -0,0 +1,98 @@
+package com.touchpo.android.meetup.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.touchpo.android.dotykacka_meetup.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * @author kocopepo
+ *         created on 01.05.16
+ */
+public class MeetupAdapter extends RecyclerView.Adapter<MeetupAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<MeetupTopic> mList;
+    private MeetupItemActionListener mListener;
+
+    public MeetupAdapter(Context context) {
+        mContext = context;
+        mList = new ArrayList<>();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_meetup, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        MeetupTopic topic = mList.get(position);
+        holder.title.setText(topic.title);
+        holder.description.setText(topic.shortDescription);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public MeetupTopic getMeetupTopic(int position) {
+        return mList.get(position);
+    }
+
+    public void setListener(MeetupItemActionListener listener) {
+        mListener = listener;
+    }
+
+    public class ViewHolder
+            extends RecyclerView.ViewHolder
+            implements View.OnClickListener
+    {
+
+        @BindView(R.id.title)
+        TextView title;
+        @BindView(R.id.description)
+        TextView description;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mListener != null) {
+                mListener.onClickListener(getLayoutPosition());
+            }
+        }
+    }
+
+    public static class MeetupTopic {
+        public String title;
+        public String shortDescription;
+        public Class activityClass;
+
+        public MeetupTopic(String title, String description, Class clz) {
+            this.title = title;
+            shortDescription = description;
+            activityClass = clz;
+        }
+    }
+
+    public interface MeetupItemActionListener{
+        void onClickListener(int position);
+    }
+}
