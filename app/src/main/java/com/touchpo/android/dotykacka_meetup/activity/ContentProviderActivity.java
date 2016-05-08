6443a717e33e39d4ee07c57bc5164c5ab21d13ed
kocopepo <martinkocour1102@gmail.com>
2016-05-08 15:37:39 +0200

    Load cursor in AsyncTask.

--- app/src/main/java/com/touchpo/android/dotykacka_meetup/activity/ContentProviderActivity.java
+++ app/src/main/java/com/touchpo/android/dotykacka_meetup/activity/ContentProviderActivity.java
@@ -1,15 +1,16 @@
 package com.touchpo.android.dotykacka_meetup.activity;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.touchpo.android.dotykacka_meetup.R;
 import com.touchpo.android.dotykacka_meetup.provider.DotykackaMeetupContract.Employee;
-import com.touchpo.android.dotykacka_meetup.provider.DotykackaMeetupContract.Employee.EmployeeColumns;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,12 +27,32 @@ public class ContentProviderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_provider);
         ButterKnife.bind(this);
+        LoadEmployeesTask loadTask = new LoadEmployeesTask(this);
+        loadTask.execute();
+    }
 
-        ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(Uri.parse(Employee.CONTENT_URI), null, null, null, null);
-        if (cursor != null && cursor.moveToFirst() && cursor.getCount() > 0) {
-            mEmployeeName.setText(cursor.getString(cursor.getColumnIndex(EmployeeColumns.COL_EMPLOYEE)));
-            mEmployeeRole.setText(cursor.getString(cursor.getColumnIndex(EmployeeColumns.COL_ROLE)));
+    private class LoadEmployeesTask extends AsyncTask<Void, Void, Cursor> {
+
+        private Context mContext;
+
+        public LoadEmployeesTask(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... params) {
+            ContentResolver resolver = getContentResolver();
+            Cursor cursor = resolver.query(Uri.parse(Employee.CONTENT_URI), null, null, null, null);
+            return cursor;
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            super.onPostExecute(cursor);
+            if (cursor != null && cursor.moveToFirst() && cursor.getCount() > 0) {
+                mEmployeeName.setText(cursor.getString(cursor.getColumnIndex(Employee.EmployeeColumns.COL_EMPLOYEE)));
+                mEmployeeRole.setText(cursor.getString(cursor.getColumnIndex(Employee.EmployeeColumns.COL_ROLE)));
+            }
         }
     }
 }
